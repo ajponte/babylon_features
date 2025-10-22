19e1f7ec0b912ac7ba7d8304042dc78eb120b07f
Alan Ponte <alanjponte@gmail.com>
2025-10-22 01:05:04 -0700

    remove unused service obj

--- features_pipeline/processor.py
+++ features_pipeline/processor.py
@@ -1,15 +1,11 @@
 """Process data lake records."""
 from abc import ABC, abstractmethod
 
-import os
-
-from datetime import timezone, datetime
-from sympy.multipledispatch.dispatcher import source
+from langchain_core.documents import Document
 
 from datalake.repository import TransactionRepository, BaseRepository, TransactionDto
 from features_pipeline.logger import get_logger
 from features_pipeline.rag.documents.document_builder import build_langchain_document
-from features_pipeline.rag.documents.document_manager import RagCollection
 from features_pipeline.rag.vectorstore import VectorStore
 
 _LOGGER = get_logger()
@@ -58,54 +54,20 @@ class CollectionProcessor(DataLakeProcessor):
         self.__update_batch_number()
         collection_name = transaction_repository.collection.name
         transactions: list[TransactionDto] = transaction_repository.get_by_filter({})
-        rag_collection = self.__build_rag_collection()
+        # The langchain docs we want to vectorize.
+        documents: list[Document] = []
         for transaction in transactions:
-            _LOGGER.info(
-                f"Adding new RAG document to RagCollection {rag_collection.pid}"
-            )
             try:
-                rag_collection.add_documents(
+                documents.extend(
                     [build_langchain_document(source=transaction, collection=collection_name)]
                 )
             except Exception as e:
                 message = f'Error while building langchain document for record {transaction.id}'
                 _LOGGER.info(message)
+                _LOGGER.debug(f'Error: {e}')
                 continue
 
-        rag_collection.vectorize(self._vector_store)
-
-    def __build_rag_collection(self) -> RagCollection:
-        """
-        Return a new `RagCollection` object. This method should be called when a new
-        document parsing process is needed. The intention of this interface is to manage
-        the start of a new document parsing process, which is compute-intensive. As such,
-        The PID and start time as a UTC int is recorded in the instantiation of the object.
-
-        :return: A new `RagCollection`.
-        """
-
-        def set_rag_process(
-            pid: int, start_ts: float, documents: list
-        ) -> RagCollection:
-            """
-            Set the start time and current PID for a RAG process.
-
-
-            :param pid: Current python PID.
-            :param start_ts: PID ts start.
-            :param documents: Documents to vectorize.
-            """
-            if len(documents) > 0:
-                raise ValueError(
-                    f"Documents must be empty to create a new RAGCollection: {len(documents)}"
-                )
-
-            _LOGGER.debug(f"Starting RAG process: {pid} at {now}")
-            return RagCollection(pid=pid, _start_ts=start_ts, documents=documents)
-
-        # Create new `RagCollection` for this python process.
-        now = datetime.now().astimezone(timezone.utc)
-        return set_rag_process(pid=os.getpid(), start_ts=now.timestamp(), documents=[])
+        self._vector_store.add_documents(documents)
 
     def __update_batch_number(self) -> None:
         _LOGGER.info(f'Bumping up batch number to {self._batch_number}')
