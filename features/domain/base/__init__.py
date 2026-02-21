3de043824128f6b82e4f14316d4ed2ddc211bb6a
Alan Ponte <alanjponte@gmail.com>
2026-02-21 12:20:59 -0800

    cleanup after rebase

--- features/domain/base/__init__.py
+++ features/domain/base/__init__.py
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 """Babylon Base Domain Entities."""
 
 from .vector import BabylonVectorBasedDocument
 
 __all__ = ["BabylonVectorBasedDocument"]
-=======
-from .vector import BabylonVectorBasedDocument
-__all__ = ["BabylonVectorBasedDocument"]
->>>>>>> de92a65 (add vectorstore querying with pydantic classes)
