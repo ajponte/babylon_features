46f6b9904dd188b20ffa2805c627634564c9a944
Alan Ponte <alanjponte@gmail.com>
2026-02-21 00:28:31 -0800

    Add tests

---
+++ tests/steps/test_chunk_and_embed.py
@@ -0,0 +1,8 @@
+from features_pipeline.steps.feature_generation.chunk_and_embed import chunk_and_embed
+
+def test_chunk_and_embed():
+    """Test the chunk_and_embed step."""
+    docs = [{"id": 1, "text": "cleaned content"}]
+    # Current implementation returns empty list (todo)
+    result = chunk_and_embed.entrypoint(cleaned_documents=docs)
+    assert result == []
