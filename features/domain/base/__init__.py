3fdc1b95d9c94dc3acc2073eecf30b0b590ca457
Alan Ponte <alanjponte@gmail.com>
2026-02-21 03:35:28 -0800

    add vectorstore querying with pydantic classes

--- features/domain/base/__init__.py
+++ features/domain/base/__init__.py
@@ -1,5 +1,10 @@
+<<<<<<< HEAD
 """Babylon Base Domain Entities."""
 
 from .vector import BabylonVectorBasedDocument
 
 __all__ = ["BabylonVectorBasedDocument"]
+=======
+from .vector import BabylonVectorBasedDocument
+__all__ = ["BabylonVectorBasedDocument"]
+>>>>>>> de92a65 (add vectorstore querying with pydantic classes)
